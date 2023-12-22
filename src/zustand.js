c4959ba54cbc64cdb4361471180daaf4de13da0a
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-12-22 18:56:42 +0530

    Added Global States

---
+++ src/zustand.js
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+// false = light, true = dark
+const useTheme = create((set) => ({
+  theme: false,
+  setTheme: (theme) => set({ theme }),
+}));
+
+const useDisplay = create((set) => ({
+  grouping: "",
+  ordering: "",
+  setGrouping: (grouping) => set({ grouping }),
+  setOrdering: (ordering) => set({ ordering }),
+}));
+
+export { useTheme, useDisplay };
