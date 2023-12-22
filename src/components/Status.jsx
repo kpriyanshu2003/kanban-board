b90bf8d6c01aa6d4fdc66946f9953bd8b2eb870d
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-12-23 00:48:02 +0530

    Status Page Complete

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -3,6 +3,11 @@ import { TbCircleDotted } from "react-icons/tb";
 import { GoPlus } from "react-icons/go";
 import { PiDotsThree } from "react-icons/pi";
 import Ticket from "./Ticket";
+import { TbCircle } from "react-icons/tb";
+import { BiSolidCircleThreeQuarter } from "react-icons/bi";
+import { GoCheckCircleFill } from "react-icons/go";
+import { GoXCircleFill } from "react-icons/go";
+import { useDisplay } from "../functions/zustand";
 
 function Status(props) {
   const [data, setData] = useState({
@@ -14,32 +19,54 @@ function Status(props) {
     cancelled: [],
   });
 
+  const ordering = useDisplay((state) => state.ordering);
+
   useEffect(() => {
     if (props.data) {
-      setData({
+      const sortedData = {
         data: props.data,
-        backlog: props.data.tickets.filter(
-          (ticket) => ticket.status === "Backlog"
+        backlog: sortTicketsByOrdering(
+          props.data.tickets.filter((ticket) => ticket.status === "Backlog"),
+          ordering
         ),
-        todo: props.data.tickets.filter((ticket) => ticket.status === "Todo"),
-        inProgress: props.data.tickets.filter(
-          (ticket) => ticket.status === "In progress"
+        todo: sortTicketsByOrdering(
+          props.data.tickets.filter((ticket) => ticket.status === "Todo"),
+          ordering
         ),
-        done: props.data.tickets.filter((ticket) => ticket.status === "Done"),
-        cancelled: props.data.tickets.filter(
-          (ticket) => ticket.status === "Cancelled"
+        inProgress: sortTicketsByOrdering(
+          props.data.tickets.filter(
+            (ticket) => ticket.status === "In progress"
+          ),
+          ordering
         ),
-      });
+        done: sortTicketsByOrdering(
+          props.data.tickets.filter((ticket) => ticket.status === "Done"),
+          ordering
+        ),
+        cancelled: sortTicketsByOrdering(
+          props.data.tickets.filter((ticket) => ticket.status === "Cancelled"),
+          ordering
+        ),
+      };
+      setData(sortedData);
     }
-  }, [props.data]);
+  }, [props.data, ordering]);
+
+  const sortTicketsByOrdering = (tickets, ordering) => {
+    if (ordering === "Priority")
+      return tickets.sort((a, b) => a.priority - b.priority);
+    else if (ordering === "Title")
+      return tickets.sort((a, b) => a.title.localeCompare(b.title));
+    else return tickets;
+  };
 
   return (
     <div className="flex">
-      <div className="w-76">
+      <div className="w-72 mx-2">
         <div className="flex justify-between items-center">
           <div className="flex gap-2 items-center">
             <TbCircleDotted />
-            <span>Backlog</span>
+            <span className="font-medium">Backlog</span>
             <span>{data.backlog.length}</span>
           </div>
           <div className="flex gap-2 items-center text-gray-700">
@@ -55,16 +82,18 @@ function Status(props) {
                 title={ticket.title}
                 tag={ticket.tag[0]}
                 priority={ticket.priority}
+                userId={ticket.userId}
+                users={props.data.users}
               />
             </div>
           ))}
         </div>
       </div>
-      <div className=" w-72">
+      <div className="w-72 mx-2">
         <div className="flex justify-between items-center">
           <div className="flex gap-2 items-center">
-            <TbCircleDotted />
-            <span>ToDo</span>
+            <TbCircle />
+            <span className="font-medium">ToDo</span>
             <span>{data.todo.length}</span>
           </div>
           <div className="flex gap-2 items-center text-gray-700">
@@ -80,16 +109,20 @@ function Status(props) {
                 title={ticket.title}
                 tag={ticket.tag[0]}
                 priority={ticket.priority}
+                userId={ticket.userId}
+                users={props.data.users}
               />
             </div>
           ))}
         </div>
       </div>
-      <div className=" w-72">
+      <div className="w-72 mx-2">
         <div className="flex justify-between items-center">
           <div className="flex gap-2 items-center">
-            <TbCircleDotted />
-            <span>In Progress</span>
+            <span className="text-yellow-300">
+              <BiSolidCircleThreeQuarter />
+            </span>
+            <span className="font-medium">In Progress</span>
             <span>{data.inProgress.length}</span>
           </div>
           <div className="flex gap-2 items-center text-gray-700">
@@ -105,16 +138,20 @@ function Status(props) {
                 title={ticket.title}
                 tag={ticket.tag[0]}
                 priority={ticket.priority}
+                userId={ticket.userId}
+                users={props.data.users}
               />
             </div>
           ))}
         </div>
       </div>
-      <div className=" w-72">
+      <div className="w-72 mx-2">
         <div className="flex justify-between items-center">
           <div className="flex gap-2 items-center">
-            <TbCircleDotted />
-            <span>Done</span>
+            <span className="text-blue-800">
+              <GoCheckCircleFill />
+            </span>
+            <span className="font-medium">Done</span>
             <span>{data.done.length}</span>
           </div>
           <div className="flex gap-2 items-center text-gray-700">
@@ -130,16 +167,20 @@ function Status(props) {
                 title={ticket.title}
                 tag={ticket.tag[0]}
                 priority={ticket.priority}
+                userId={ticket.userId}
+                users={props.data.users}
               />
             </div>
           ))}
         </div>
       </div>
-      <div className="w-76">
+      <div className="w-72 mx-2">
         <div className="flex justify-between items-center">
           <div className="flex gap-2 items-center">
-            <TbCircleDotted />
-            <span>Cancelled</span>
+            <span className="text-gray-400">
+              <GoXCircleFill />
+            </span>
+            <span className="font-medium">Cancelled</span>
             <span>{data.cancelled.length}</span>
           </div>
           <div className="flex gap-2 items-center text-gray-700">
@@ -155,6 +196,8 @@ function Status(props) {
                 title={ticket.title}
                 tag={ticket.tag[0]}
                 priority={ticket.priority}
+                userId={ticket.userId}
+                users={props.data.users}
               />
             </div>
           ))}
