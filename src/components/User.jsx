894df556660b7d7de80b4dbb3cf8e61ae0dd92bb
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-12-23 03:40:40 +0530

    Created User Page

---
+++ src/components/User.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import UserAvatar from "./UserAvatar";
+import { GoPlus } from "react-icons/go";
+import { PiDotsThree } from "react-icons/pi";
+import Ticket from "./Ticket";
+import { useDisplay } from "../functions/zustand";
+
+function User({ data }) {
+  const [userData, setUserData] = useState({
+    data: null,
+    users: [],
+  });
+
+  // Use the ordering value from useDisplay hook
+  const ordering = useDisplay((state) => state.ordering);
+
+  useEffect(() => {
+    if (data) {
+      setUserData({ data, users: data.users });
+    }
+  }, [data]);
+
+  const sortTickets = (userTickets) => {
+    // Ensure you create a copy of the array before sorting
+    return [...userTickets].sort((a, b) => {
+      if (ordering === "Priority") {
+        return a.priority - b.priority;
+      } else if (ordering === "Title") {
+        return a.title.localeCompare(b.title);
+      }
+      return 0;
+    });
+  };
+
+  return (
+    <div className="flex">
+      {userData.users.length > 0 ? (
+        userData.users.map((user) => (
+          <div key={user.id} className="flex w-72 mx-3">
+            <div>
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                  <UserAvatar user={user} />
+                  <span>{user.name}</span>
+                  <span>{getTicketCount(user.id)}</span>
+                </div>
+                <div className="flex gap-2 items-center text-gray-700">
+                  <GoPlus />
+                  <PiDotsThree />
+                </div>
+              </div>
+
+              <div>
+                {sortTickets(
+                  userData.data.tickets.filter(
+                    (ticket) => ticket.userId === user.id
+                  )
+                ).map((ticket) => (
+                  <Ticket
+                    key={ticket.id}
+                    id={ticket.id}
+                    title={ticket.title}
+                    tag={ticket.tag[0]}
+                    priority={ticket.priority}
+                    userId={ticket.userId}
+                    status={ticket.status}
+                    users={userData.users}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No users available.</p>
+      )}
+    </div>
+  );
+
+  function getTicketCount(userId) {
+    return userData.data.tickets.filter((ticket) => ticket.userId === userId)
+      .length;
+  }
+}
+
+export default User;
